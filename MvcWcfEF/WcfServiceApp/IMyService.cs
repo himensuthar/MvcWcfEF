cbafd1288ce6e81d6fa35754e7532a60cf346a86
himensuthar <himenmsuthar@gmail.com>
2017-10-29 18:50:04 +0530

    Creating an WCF Application - Add MyService.sc WCF Service - Create a UserDetail database table - Create EF Model from Database - Add CRUD Operation to MyServiceClass - Add Contract to IMyService Interface - ServiceContract,DataContract,OperationContract

---
+++ MvcWcfEF/WcfServiceApp/IMyService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+
+namespace WcfServiceApp
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name 
+    //       "MyService" in code, svc and config file together.  
+    // NOTE: In order to launch WCF Test Client for testing this service, please select 
+    //       MyService.svc or MyService.svc.cs at the Solution Explorer and start debugging.
+    [ServiceContract]
+    public interface IMyService
+    {
+        [OperationContract]
+        List<UserDetail> GetAllUser();
+        [OperationContract]
+        int AddUser(string Name, string Email);
+        [OperationContract]
+        UserDetail GetAllUserById(int id);
+
+        [OperationContract]
+        int UpdateUser(int Id, string Name, string Email);
+
+        [OperationContract]
+        int DeleteUserById(int Id);
+    }
+
+    [DataContract]
+    public class UserDetails
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+    }
+
+}
---
+++ MvcWcfEF/WcfServiceApp/MyService.svc.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceApp
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MyService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select MyService.svc or MyService.svc.cs at the Solution Explorer and start debugging.
+    public class MyService : IMyService
+    {
+        public List<UserDetail> GetAllUser()
+        {
+            List<UserDetail> userlst = new List<UserDetail>();
+            TestDBEntities tstDb = new TestDBEntities();
+            var lstUsr = from k in tstDb.UserDetails select k;
+            foreach (var item in lstUsr)
+            {
+                UserDetail usr = new UserDetail();
+                usr.Id = item.Id;
+                usr.Name = item.Name;
+                usr.Email = item.Email;
+                userlst.Add(usr);
+
+            }
+
+            return userlst;
+        }
+
+        public UserDetail GetAllUserById(int id)
+        {
+
+            TestDBEntities tstDb = new TestDBEntities();
+            var lstUsr = from k in tstDb.UserDetails where k.Id == id select k;
+            UserDetail usr = new UserDetail();
+            foreach (var item in lstUsr)
+            {
+
+                usr.Id = item.Id;
+                usr.Name = item.Name;
+                usr.Email = item.Email;
+
+
+            }
+
+            return usr;
+        }
+
+        public int DeleteUserById(int Id)
+        {
+
+            TestDBEntities tstDb = new TestDBEntities();
+            UserDetail usrdtl = new UserDetail();
+            usrdtl.Id = Id;
+            tstDb.Entry(usrdtl).State = EntityState.Deleted;
+            int Retval = tstDb.SaveChanges();
+            return Retval;
+        }
+
+        public int AddUser(string Name, string Email)
+        {
+            TestDBEntities tstDb = new TestDBEntities();
+            UserDetail usrdtl = new UserDetail();
+            usrdtl.Name = Name;
+            usrdtl.Email = Email;
+            tstDb.UserDetails.Add(usrdtl);
+            int Retval = tstDb.SaveChanges();
+            return Retval;
+        }
+
+        public int UpdateUser(int Id, string Name, string Email)
+        {
+            TestDBEntities tstDb = new TestDBEntities();
+            UserDetail usrdtl = new UserDetail();
+            usrdtl.Id = Id;
+            usrdtl.Name = Name;
+            usrdtl.Email = Email;
+            tstDb.Entry(usrdtl).State = EntityState.Modified;
+            int Retval = tstDb.SaveChanges();
+            return Retval;
+        }
+
+    }
+}
